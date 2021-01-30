8c72f02a4bb0dce9d1bac62a4abc320cbbd80304
rynvva <rynvva@gmail.com>
2021-01-30 20:32:11 +0700

    initial setup

--- pages/index.js
+++ pages/index.js
@@ -1,65 +1,77 @@
+import { useEffect, useState } from 'react'
+
 import Head from 'next/head'
+import { useSpring, animated } from 'react-spring'
+
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
+  const [contributors, setContributors] = useState(['', '', '', '', '', '', '', ''])
+  const [hoveringAtIndex, setHoveringAtIndex] = useState('')
+  
   return (
-    <div className={styles.container}>
+    <>
       <Head>
-        <title>Create Next App</title>
+        <title>Home</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+      <main>
+        <div className={styles.container}>
+          <div className={styles.topNavContainer}>
+            <div className={styles.topNav}>
+              <a href='/'>
+                <h2 className={styles.topNavTitle}>
+                  CrocoBoard
+                </h2>
+              </a>
+            </div>
+          </div>
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          <div className={styles.contributorsListContainer}>
+            <div className={styles.contributorsListInnerContainer}>
+              {
+                contributors.map((contributor, contributorIndex) => (
+                  <Contributor
+                    contributorIndex = {contributorIndex}
+                    opacity = {hoveringAtIndex == '' || hoveringAtIndex == contributorIndex ? 1 : 0.3}
+                    onMouseEnter={() => setHoveringAtIndex(contributorIndex.toString())}
+                    onMouseLeave={() => setHoveringAtIndex('')}
+                  />
+                ))
+              }
+            </div>
+          </div>
         </div>
       </main>
+    </>
+  )
+}
 
-      <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
-      </footer>
-    </div>
+function Contributor(props) {
+  const { contributorIndex, onMouseEnter, onMouseLeave, opacity } = props
+
+  const animation = useSpring({ opacity: opacity == 1 ? 1.0 : 0.2, config: {duration: 500}, from: { opacity: opacity == 1 ? 0.2 : 1 } })
+
+  return (
+    <animated.a
+      className={styles.contributor}
+      href='/#'
+      key = {contributorIndex}
+      onMouseEnter={onMouseEnter}
+      onMouseLeave={onMouseLeave}
+      style = {{
+        opacity: animation.opacity
+      }}
+    >
+      <img
+        className={styles.contributorPhoto}
+        src='https://images.unsplash.com/photo-1584799235813-aaf50775698c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NXx8bWFufGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60'
+      />
+
+      <div className={styles.contributorName}>
+        Andrew {contributorIndex}
+      </div>
+    </animated.a>
   )
 }
